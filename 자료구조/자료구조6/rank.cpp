e23464a5f620d332e038dff243762cf16fad236d
asd147asd147 <ds5vfr@gmail.com>
2019-09-29 15:31:53 +0900

    자료구조

--- 자료구조/자료구조6/rank.cpp
+++ 자료구조/자료구조6/rank.cpp
@@ -3,46 +3,67 @@
 using namespace std;
 typedef struct { string name ;  vector<int> report; }  Stud ;
 
+
+ifstream in("rank.inp");
+ofstream out("rank.out");
+
 bool myfun ( Stud a, Stud b)   {
+    if((a.report.size())==( b.report.size())){
+        if((a.report.back())==(b.report.back())){
+            for(auto i = 0; i<a.report.size()-1;i++){
+                if(*(a.report.begin()+i) > *(b.report.begin()+i)){
+                    return 1;
+                }
+                else if(*(a.report.begin()+i) < *(b.report.begin()+i)){
+                    return 0;
+                }
+            }
+            if((a.name).compare(b.name) < 0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        return ((a.report.back())>(b.report.back()));
+    }
     return ((a.report.size())>( b.report.size())) ;
-} // 비교 함수
+}
 
 void outclass( vector < Stud > Pclass ){
     for(auto s : Pclass) {
-        cout << "\n name= " << s.name ;
-        for( auto t: s.report)
-            cout << " " << t ;
+        out << s.name <<"\n";
     }
 }
 
 int main() {
 
     int i, N, score ;
+    int sum = 0;
     string xname ;
     Stud   body ;
     vector < Stud > Pclass ;
 
-    ifstream in("rank01.txt");
 
-    in >> N ; cout << "\n N= " << N ;
+    in >> N ;
 
     for(i=0 ; i < N ; i++){
-        in >> xname ; cout << "\n name= " << xname ;
+        in >> xname ;
         body.name = xname ;
         body.report.clear();
+        sum = 0;
         while(1){
             in >> score ;
+            sum += score ;
             if( score <= 0) break ;
             else {
                 body.report.push_back( score ) ;
-                cout << " " << score ;
             }
         } // end of while()
+        body.report.push_back( sum ) ;
         sort( body.report.begin(),  body.report.end() ) ;
         Pclass.push_back( body );
     } // end of for()
-
-    cout << "\n\n After Sorting() ------ \n" ;
     sort( Pclass.begin(), Pclass.end(), myfun ) ;
     outclass( Pclass ) ;
 
