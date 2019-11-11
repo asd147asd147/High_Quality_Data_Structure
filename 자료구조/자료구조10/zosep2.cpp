41c718aef462d32ad0a8ea612e9a11840720ea9d
asd147asd147 <asd147asd147@naver.com>
2019-11-11 17:49:48 +0900

    자료구조

--- 자료구조/자료구조10/zosep2.cpp
+++ 자료구조/자료구조10/zosep2.cpp
@@ -4,68 +4,105 @@
 
 using namespace std;
 
-struct data{
-    int num;
-    int mod;
-};
 int main(){
 
-    ifstream in("3.inp");
+    ifstream in("zosep.inp");
     ofstream out("zosep.out");
 
-    list <data> zos;
+    list <int> zos;
 
     int start,finish;
-    int NUM;
-    data temp;
     in >> start >> finish;
-
-    NUM = finish - start;
-    temp.num = start;
-    temp.mod = temp.num%3;
-    zos.push_back(temp);
-    for(int i = 1; i <= NUM; ++i){
-        ++temp.num;
-        ++temp.mod;
-        if(temp.mod == 3){
-            temp.mod = 0;
+    int NUM = finish-start+1;
+    int last = start;
+    int stage = 0, flag;
+    if(start % 3 == 1){
+        for(int i = 1; i <= NUM; ++i){
+            if(i % 3 != 1){
+                zos.push_back(i);
+            }
         }
-        zos.push_back(temp);
+        stage = 1;
+        last -=1;
+    }
+    else if(start % 3 == 2){
+        int j = 2;
+        flag = 1;
+        for(int i = 2; i <= NUM+1; ++i){
+            if(i != j){
+                zos.push_back(i);
+            }
+            else{
+                flag ? j+=4 : j+=2;
+                flag = !flag;
+            }
+            if(NUM+1 == j) stage =2;
+        }
+        stage += 2;
+        last -=2;
+    }
+    else{
+        int j = 3;
+        flag = 0;
+        for(int i = 3; i <= NUM+2; ++i){
+            if(i != j){
+                zos.push_back(i);
+            }
+            else{
+                flag ? j+=4 : j+=2;
+                flag = !flag;
+            }
+            if(NUM+2 == j) stage = 2;
+        }
+        stage += 2;
+        last -=3;
     }
 
-    int cnt = 0;
-    int jum;
-    for(auto i = zos.begin(); cnt != NUM; ++cnt){
-        jum = i->mod+1;
-        i = zos.erase(i);
-        switch (jum){
-        case 3:
-            if(i == zos.end()){
-                ++i;
-            }
-            ++i;
-            if(i == zos.end()){
-                ++i;
-            }
-        case 2:
-            if(i == zos.end()){
-                ++i;
-            }
-            ++i;
-            if(i == zos.end()){
-                ++i;
-            }
+    list<int>::iterator iter = zos.begin();
+    if(stage == 1){
+        switch(finish%3){
         case 1:
-            if(i == zos.end()){
-                ++i;
-            }
-            ++i;
-            if(i == zos.end()){
-                ++i;
-            }
+            ++iter;
+        case 2:
+            ++iter;
         }
     }
-    out << (*zos.begin()).num;
+    else if(stage == 2){
+        switch(finish%3 + flag){
+        case 0:
+            ++iter;
+        case 1:
+            ++iter;
+        }
+    }
+    else if(stage == 4){
+        switch(finish%3+flag){
+        case 2:
+            ++iter;
+        case 0:
+            ++iter;
+        case 1:
+            ++iter;
+        }
+    }
+    int temp;
+    while(zos.size() != 1){
+        temp = (*iter)%3;
+        iter = zos.erase(iter);
+        if(iter == zos.end()) ++iter;
+        switch(temp){
+        case 2:
+            ++iter;
+            if(iter == zos.end()) ++iter;
+        case 1:
+            ++iter;
+            if(iter == zos.end()) ++iter;
+        case 0:
+            ++iter;
+            if(iter == zos.end()) ++iter;
+        }
+    }
+    cout << *zos.begin()+last;
 
     return 0;
 }
