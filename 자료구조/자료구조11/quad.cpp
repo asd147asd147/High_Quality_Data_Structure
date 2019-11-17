7b82aad50eb3348332ded08e6f36d998675a03c5
asd147asd147 <ds5vfr@gmail.com>
2019-11-17 18:33:13 +0900

    자료구조

--- 자료구조/자료구조11/quad.cpp
+++ 자료구조/자료구조11/quad.cpp
@@ -1,115 +1,108 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int size = 4;
-typedef struct tree{
-    int data;
-    tree* ptr[size];
-} TREE;
-
-ifstream in("test.inp");
+ifstream in("quad.inp");
 ofstream out("quad.out");
 
-vector<vector<int>> code;
-vector<int>sort_code;
+typedef struct tree{
+    int key;
+    tree* ptr[4];
+} TREE;
+
+vector<vector<int>> data;
+
+vector<int> sort_data;
 
 TREE* root = new TREE;
 
-int quad_size;
-int depth;
+int n;
+int line = 1;
 
 void input(){
-    char c;
-    in >> quad_size;
-    depth = quad_size+1;
-    for(int i = 0; i < 1<<quad_size; i++){
+    in>>n;
+    line = line << n;
+    for(int i = 0; i < line; i++){
         vector<int> temp;
-        for(int j = 0; j < 1<<quad_size; j++){
+        for(int j = 0; j < line; j++){
+            char c;
             in >> c;
-            if(c == '#')
-                temp.push_back(1);
-            else
-                temp.push_back(0);
+            if(c == '#') temp.push_back(1);
+            else temp.push_back(0);
         }
-        code.push_back(temp);
+        data.push_back(temp);
     }
 }
 
-void sort_node(){
-    int line1 = 0, line2 = 1;
-    for(int i = 0;i < 1 << quad_size-1; i++){
-        for(int j = 0;j < 1 << quad_size;j+=2){
-            sort_code.push_back(code[line1][j]);
-            sort_code.push_back(code[line1][j+1]);
-            sort_code.push_back(code[line2][j]);
-            sort_code.push_back(code[line2][j+1]);
-        }
-        line1+=2;
-        line2+=2;
-    }
-}
-
-void make_leaf(TREE* node, int d){
-    if(d != 1){
-        node->data = 2;
-        for(int i = 0; i < size; i++){
-            node->ptr[i] = new TREE;
-        }
-        for(int i = 0; i < size; i++){
-            make_leaf(node->ptr[i], d-1);
-        }
+void data_sort(int start_x, int start_y, int size){
+    if(size == 1){
+        sort_data.push_back(data[start_x][start_y]);
     }
     else{
-        node->data = 2;
-        for(int i = 0; i < size; i++){
-            node->ptr[i] = NULL;
-        }
+        data_sort(start_x,start_y,size/2);
+        data_sort(start_x,start_y-(size/2),size/2);
+        data_sort(start_x+(size/2),start_y-(size/2),size/2);
+        data_sort(start_x+(size/2),start_y,size/2);
     }
 }
 
-int node_index = 0;
-void push_node(TREE* node){
-    for(int i = 0; i < size; i++){
-        if(node->ptr[i] != NULL){
-            push_node(node->ptr[i]);
-        }
-        else{
-            node->data = sort_code[node_index];
-            node_index++;
-            break;
-        }
+int leaf_index = 0;
+void make_tree(TREE* node, int size){
+    if(size == 1){
+        node->key = sort_data[leaf_index];
+        for(int i = 0; i < 4; i++)
+            node->ptr[i] = NULL;
+        leaf_index++;
     }
-
+    else{
+        node->key = 2;
+        for(int i = 0; i < 4; i++)
+            node->ptr[i] = new TREE;
+        for(int i = 0; i < 4; i++)
+            make_tree(node->ptr[i],size/2);
+    }
 }
 
 int reduce_tree(TREE* node){
-    if(node->data != 2){
-        return node->data;
+    if(node->ptr[0] != NULL){
+        int n0,n1,n2,n3;
+        n0 = reduce_tree(node->ptr[0]);
+        n1 = reduce_tree(node->ptr[1]);
+        n2 = reduce_tree(node->ptr[2]);
+        n3 = reduce_tree(node->ptr[3]);
+        if(n0 == n1 && n2 == n3 && n0 == n2){
+            if(n0 != 2){
+                node->key = n0;
+                for(int i = 0; i < 4; i++)
+                    node->ptr[i] = NULL;
+                return n0;
+            }
+            else{
+                return 2;
+            }
+        }
+        return 2;
     }
-    else if(reduce_tree(node->ptr[0]) == reduce_tree(node->ptr[1]) && reduce_tree(node->ptr[2]) == reduce_tree(node->ptr[3]) && node->ptr[0]->data == node->ptr[2]->data){
-        node->data = node->ptr[0]->data;
-        for(int i = 0; i < size; i++)
-            node->ptr[i] = NULL;
+    else{
+        return node->key;
+    }
+}
+
+void print_tree(TREE* node){
+    if(node->ptr[0] == NULL){
+        out << node->key << " ";
+    }
+    else{
+        out << node->key << " ( ";
+        for(int i = 0; i < 4; i++)
+            print_tree(node->ptr[i]);
+        out << ") ";
     }
 }
 
 void solve(){
-    make_leaf(root, depth);
-    sort_node();
-    push_node(root);
-    //reduce_tree(root);
-}
-
-void print_tree(TREE* node){
-    out << node->data << " ";
-    if(node->data == 2){
-        out << "(" << " ";
-        print_tree(node->ptr[1]);
-        print_tree(node->ptr[0]);
-        print_tree(node->ptr[2]);
-        print_tree(node->ptr[3]);
-        out << ")" << " ";
-    }
+    data_sort(0,line-1,line);
+    make_tree(root,line);
+    reduce_tree(root);
 }
 
 void output(){
@@ -120,6 +113,7 @@ int main() {
     input();
     solve();
     output();
+
     return 0;
 }
 
