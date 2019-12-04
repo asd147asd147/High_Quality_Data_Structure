6f8ae3bc6966620d69f13a621b93e5dfc6ce25b6
asd147asd147 <asd147asd147@naver.com>
2019-12-04 17:47:52 +0900

    자료구조

--- 자료구조/자료구조13/citypath.cpp
+++ 자료구조/자료구조13/citypath.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 #include <algorithm>
 
+#define INF 99999
+
 using namespace std;
 
 ifstream in("1.inp");
@@ -12,10 +14,10 @@ ofstream out("citypath.out");
 int NODENUM;
 
 vector<vector<int>> NODEDATA;
+vector<vector<int>> WEIGHT;
 
 void input(){
     in >> NODENUM;
-
     int temp;
     for(int i = 0; i < NODENUM; ++i){
         vector <int> tempdata;
@@ -26,15 +28,34 @@ void input(){
         }
         NODEDATA.push_back(tempdata);
     }
+    for(int i = 0; i < NODENUM; ++i){
+        vector <int> tempdata;
+        for(int j = 0; j < NODENUM; ++j){
+            tempdata.push_back(INF);
+        }
+        WEIGHT.push_back(tempdata);
+    }
 }
 
+void input_weight(){
+    for(int i = 0; i < NODENUM; ++i){
+        for(int j = 0; j < NODEDATA[i].size(); ++j){
+            if(i == NODEDATA[i][j]-1){
+                WEIGHT[i][i] = 0;
+            }
+            else{
+                WEIGHT[i][NODEDATA[i][j]-1] = NODEDATA[NODEDATA[i][j]-1].size()-2;
+            }
+        }
+    }
+}
 
 void solve(){
-
+    input_weight();
 }
 
 void output(){
-    for(auto s : NODEDATA){
+    for(auto s : WEIGHT){
         for(auto t : s){
             cout << t;
         }
@@ -45,6 +66,6 @@ void output(){
 int main(){
     input();
     solve();
-    //output();
+    output();
     return 0;
 }
